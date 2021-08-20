5ea0935e5e3ba55d89e911441b3e9ae917fa39f6
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-20 17:40:48 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const { Client, Collection, Intents, MessageEmbed, GuildMember } = require('discord.js');
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
+
+const prefix = process.env.PREFIX
+
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+	console.log(`${command.name}.js loaded!`);
+}
+
+client.on('ready', () => {
+	console.log('Ready!');
+    client.user.setActivity(`${prefix}help`, { type: 'PLAYING' });
+});
+
+client.on('messageCreate', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+
+	try {
+		client.commands.get(command).execute(message, args, client)
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+client.login(process.env.DISCORD_TOKEN);
