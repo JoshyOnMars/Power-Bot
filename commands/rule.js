0bdb3e9b06a645e83752c3cd97ea10b214076058
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-22 22:58:36 +0100

    Update rule.js

--- commands/rule.js
+++ commands/rule.js
@@ -6,14 +6,32 @@ module.exports = {
     	  category: 'Info',
 	  async execute(message, args, client) {
 
-        const inputThing = args[0];
+        const input = args[0];
         
-        if (inputThing === "1") {
+        if (input === "1") {
             let embed = new MessageEmbed()
             .setColor("BLUE")
-            .addField("Rule 1", "You **MUST** be over the age of 13, Underaged user will be kicked.")
+            .addField("Rule 1", "You **MUST** be over the age of 13, Underaged users will be kicked.")
             .setTimestamp()
             message.channel.send({ embeds: [embed] })
-        }
+        } else if (input === "2") {
+            let embed = new MessageEmbed()
+            .setColor("BLUE")
+            .addField("Rule 2", "All content here is PG-13, So no Adult or NSFW content. Cursing is allowed but only in <#808772107911299072>")
+            .setTimestamp()
+            message.channel.send({ embeds: [embed] })
+        } else if (input === "3") {
+            let embed = new MessageEmbed()
+            .setColor("BLUE")
+            .addField("Rule 3", "No Spamming. Don't be a nuisance or you may get muted or even kicked.")
+            .setTimestamp()
+            message.channel.send({ embeds: [embed] })
+        } else if (input === "4") {
+            let embed = new MessageEmbed()
+            .setColor("BLUE")
+            .addField("Rule 4", "Don't beg for follows, If you are sharing your social media accounts or websites, It has to be related to an ongoing discussion.")
+            .setTimestamp()
+            message.channel.send({ embeds: [embed] })
+        } 
 	},
 };
