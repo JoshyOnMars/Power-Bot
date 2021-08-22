e992ceb03f71f6312be8bea5abe0a9f3454d03be
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-22 14:15:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ client.on('messageCreate', message => {
 		client.commands.get(command).execute(message, args, client)
 	} catch (error) {
 		console.error(error);
-		message.reply('there was an error trying to execute that command!');
+		message.reply('There was an error trying to execute that command, please contact <@691634056278048778> to fix it!');
 	}
 });
 
