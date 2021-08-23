8b8efd52312d8731c1b6aacf3941a1ad612a6357
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-23 12:18:40 +0100

    Create weather.js

---
+++ commands/weather.js
@@ -0,0 +1,33 @@
+const { MessageEmbed} = require('discord.js');
+const weather = require('weather-js');
+
+module.exports = {
+    name: 'weather',
+    async execute(client, message, args) {
+    
+        weather.find({search: args.join(" "), degreeType: 'F'}, function (error, result){
+        // 'C' can be changed to 'F' for farneheit results
+        if(error) return message.channel.send(error);
+        if(!args[0]) return message.channel.send('Please specify a location')
+
+        if(result === undefined || result.length === 0) return message.channel.send('**Invalid** location');
+
+        var current = result[0].current;
+        var location = result[0].location;
+
+        const weatherinfo = new MessageEmbed()
+        .setDescription(`**${current.skytext}**`)
+        .setAuthor(`Weather forecast for ${current.observationpoint}`)
+        .setThumbnail(current.imageUrl)
+        .setColor("BLUE")
+        .addField('Timezone', `UTC${location.timezone}`, true)
+        .addField('Temperature', `${current.temperature}°`, true)
+        .addField('Wind', current.winddisplay, true)
+        .addField('Feels like', `${current.feelslike}°`, true)
+        .addField('Humidity', `${current.humidity}%`, true)
+
+
+        message.channel.send({ embeds: [weatherinfo] })
+        })        
+    }
+}
