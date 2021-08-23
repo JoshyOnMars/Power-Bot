865d884316afef75076ccd7221fd0c2b6dc0499a
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-23 14:58:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -87,6 +87,27 @@ client.on('messageCreate', async message => {
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
+	
+	if(!cooldowns.has(command.name)){
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const current_time = Date.now();
+    const time_stamps = cooldowns.get(command.name);
+    const cooldown_amount = (command.cooldown) * 1000;
+
+    if(time_stamps.has(message.author.id)){
+        const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
+
+        if(current_time < expiration_time){
+            const time_left = (expiration_time - current_time) / 1000;
+
+            return message.reply(`Please wait ${time_left.toFixed(1)} more seconds before using ${command.name}`);
+        }
+    }
+
+    time_stamps.set(message.author.id, current_time);
+    setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
 
 	try {
 		client.commands.get(command).execute(message, args, client, profileData)
