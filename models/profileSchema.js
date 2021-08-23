33e5f0f6f38b6881bbc133efde063a09b2f6be1b
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-23 14:42:28 +0100

    Update profileSchema.js

--- models/profileSchema.js
+++ models/profileSchema.js
@@ -5,7 +5,7 @@ const profileSchema = new mongoose.Schema({
   serverID: { type: String, require: true },
   coins: { type: Number, default: 1000 },
   bank: { type: Number },
-  modLogs: { type: Number },
+  modLogs: { type: Number, default: 0 },
 });
 
 const model = mongoose.model("ProfileModels", profileSchema);
