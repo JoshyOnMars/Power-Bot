f8d7bd53e3a31e94c107ca0e33c0365887afaa47
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-23 14:01:55 +0100

    Create beg.js

---
+++ commands/beg.js
@@ -0,0 +1,20 @@
+module.exports = {
+  name: "beg",
+  async execute(message, args, cmd, client, discord, profileData) {
+    const randomNumber = Math.floor(Math.random() * 500) + 1;
+    const names = ["Elon Musk", "Jeff Bezos", "Bill Gates", "Joe Biden", "Zac Aubert", "Marcus"]
+    const randomNames = names[Math.floor(Math.random() * names.legnth)];
+    
+    const response = await profileModel.findOneAndUpdate(
+      {
+        userID: message.author.id,
+      },
+      {
+        $inc: {
+          coins: randomNumber,
+        },
+      }
+    );
+    return message.channel.send(`${message.author.username}, You begged and ${randomNames} gave you ${randomNumber} coins!`);
+  },
+};
