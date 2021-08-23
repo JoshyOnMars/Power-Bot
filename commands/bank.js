bf051db0ebc7f72433c6d7c39845b6a26533fc39
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-23 17:56:20 +0100

    Create bank.js

---
+++ commands/bank.js
@@ -0,0 +1,27 @@
+
+const { MessageEmbed } = require("discord.js")
+const profileModel = require("../models/profileSchema");
+
+module.exports = {
+  name: "beg",
+  cooldown: 300,
+  async execute(message, args, client, profileData) {
+   
+    const number = args[0]
+    if (!number) { return message.channel.send(`Please specify the amount you want to deposit! (Min 1000)`)}
+    if (number > 1000) { return message.channel.send("Your amount is more than the min amount of 1000.")}
+    
+    const response = await profileModel.findOneAndUpdate(
+      {
+        userID: message.author.id,
+      },
+      {
+        $inc: {
+          coins: -number,
+          bank: number,
+        },
+      }
+    );
+  return message.channel.send(`Banked ${number} coins into your Bank!`)
+  },
+};
