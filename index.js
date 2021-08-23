9fd670aabd21e448fc9a86b520bddc0e6f9b201d
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-23 13:36:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const { Client, Collection, Intents, MessageEmbed, GuildMember } = require('discord.js');
 require("dotenv").config();
 const mongoose = require("mongoose");
+const profileModel = require("../../models/profileSchema");
 const badwordsArray = require("./badwords.js")
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
