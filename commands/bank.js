6dd0157332663599d673a4db766a29693e3feba8
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-23 18:01:12 +0100

    Update bank.js

--- commands/bank.js
+++ commands/bank.js
@@ -4,13 +4,12 @@ const profileModel = require("../models/profileSchema");
 
 module.exports = {
   name: "bank",
-  cooldown: 300,
   async execute(message, args, client, profileData) {
    
     const number = args[0]
     if (!number) { return message.channel.send(`Please specify the amount you want to deposit! (Min 1000)`)}
     if (number > 1000) { return message.channel.send("Your amount is more than the min amount of 1000.")}
-    
+    if (profileData.bank === 1000) { return message.channel.send(`You have the max amount of \`1000\` in your bank!`)}
     const response = await profileModel.findOneAndUpdate(
       {
         userID: message.author.id,
