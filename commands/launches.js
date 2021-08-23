acb64ac2ca56ec7644ebac8554ea788d625204ca
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-24 00:49:50 +0100

    Update launches.js

--- commands/launches.js
+++ commands/launches.js
@@ -6,7 +6,13 @@ module.exports = {
 	description: 'Tells you the 5 upcoming rocket launches!',
         category: 'Info',
 	async execute(message, args, client) {
-
+        
+        fetch('https://fdo.rocketlaunch.live/json/launches/next/25')
+        .then(res => res.json()) 
+        .then(json => {
+        for (const i of json.result) {
+        message.channel.send(`${i.provider.name}, ${i.vehicle.name}`);
+        }})
         
 	},
 };
