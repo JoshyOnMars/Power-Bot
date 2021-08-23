b230634b83885aeba2f046f261268360dc1b0baa
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-23 14:48:01 +0100

    Create modlogs.js

---
+++ commands/modlogs.js
@@ -0,0 +1,13 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+  name: "modlogs",
+  async execute(message, args, client, profileData) {
+    let embed = new MessageEmbed()
+    .setColor("RANDOM")
+    .setAuthor(`${message.author.username}'s Modlogs`)
+    .addField(`Modlogs`, `\`${profileData.modLogs}\``)
+    .setTimestamp()
+    message.channel.send({ embeds: [embed] });
+  },
+};
