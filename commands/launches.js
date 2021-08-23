68027377b8fe5d64d60afec561f9be99c330f5c5
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-24 00:44:56 +0100

    Create launches.js

---
+++ commands/launches.js
@@ -0,0 +1,12 @@
+const { MessageEmbed } = require('discord.js');
+const fetch = require("node-fetch");
+
+module.exports = {
+	name: 'launches',
+	description: 'Tells you the 5 upcoming rocket launches!',
+        category: 'Info',
+	async execute(message, args, client) {
+
+        
+	},
+};
