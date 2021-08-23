9e8135d51c6d2e3234e1e75e97ab0c3a2d25f43c
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-23 14:12:20 +0100

    Update beg.js

--- commands/beg.js
+++ commands/beg.js
@@ -5,8 +5,8 @@ module.exports = {
   name: "beg",
   async execute(message, args, client, profileData) {
     const randomNumber = Math.floor(Math.random() * 500) + 1;
-    const names = ["Elon Musk", "Jeff Bezos", "Bill Gates", "Joe Biden", "Zac Aubert", "Marcus"]
-    const randomNames = names[Math.floor(Math.random() * names.legnth)];
+    let names = ["Elon Musk", "Jeff Bezos", "Bill Gates", "Joe Biden", "Zac Aubert", "Marcus"]
+    let randomNames = names[Math.floor(Math.random() * names.legnth)];
     
     const response = await profileModel.findOneAndUpdate(
       {
