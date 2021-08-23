ef5c7fb09b87f049482a1b55c119197a87eeb06a
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-23 15:02:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 const prefix = process.env.PREFIX
 
 client.commands = new Collection();
+const cooldowns = new Map();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
