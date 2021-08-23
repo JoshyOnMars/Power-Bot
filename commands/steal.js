e0b17325393becacf05364bf104e50fe3795f8bd
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-23 17:48:09 +0100

    Create steal.js

---
+++ commands/steal.js
@@ -0,0 +1,34 @@
+const { MessageEmbed } = require("discord.js")
+const profileModel = require("../models/profileSchema");
+
+module.exports = {
+  name: "steal",
+  cooldown: 600,
+  async execute(message, args, client, profileData) {
+    const randomNumber = Math.floor(Math.random() * 500) + 1;
+    let mentionedUser = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+    
+    const response = await profileModel.findOneAndUpdate(
+      {
+        userID: message.author.id,
+      },
+      {
+        $inc: {
+          coins: randomNumber,
+        },
+      }
+    );
+    const response2 = await profileModel.findOneAndUpdate(
+      {
+        userID: mentionedUser.id,
+      },
+      {
+        $inc: {
+          coins: -randomNumber,
+        },
+      }
+    );
+    if (!response2) { return message.channel.send(`${mentionedUser} doesn't appear to have any money!`)}
+    return message.channel.send(`${message.author.username}, You stole ${randomNumber} coins from ${mentionedUser}!`);
+  },
+};
