b4166561771e643917f737502196121eaaea11dc
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-23 13:06:30 +0100

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -5,22 +5,22 @@ module.exports = {
     name: 'weather',
     async execute(message, args, client) {
     
-        weather.find({search: args.join(" "), degreeType: 'C'}, function (error, result){
+        weather.find({search: args.join(" ")}, function (error, result){
         // 'C' can be changed to 'F' for farneheit results
         if(error) return message.channel.send(error);
         if(!args[0]) return message.channel.send('Please specify a location')
 
         if(result === undefined || result.length === 0) return message.channel.send('**Invalid** location');
 
-        var current = result[0].current;
-        var location = result[0].location;
+        let current = result[0].current;
+        let location = result[0].location;
 
         const weatherinfo = new MessageEmbed()
-        .setDescription(`**${current.skytext}**, **Day & Time**: ${current.day}, ${current.observationtime}`)
+        .setDescription(`**${current.skytext}**, **Time**: ${current.observationtime}`)
         .setAuthor(`Weather forecast for ${current.observationpoint}`)
         .setThumbnail(current.imageUrl)
         .setColor("BLUE")
-        .addField('Temperature', `${current.temperature}°C`, true)
+        .addField('Temperature', `${current.temperature}°${location.degreetype}`, true)
         .addField('Wind', current.winddisplay, true)
         .addField('Humidity', `${current.humidity}%`, true)
 
