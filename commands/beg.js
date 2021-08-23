eb8b6bd64060105b724619ccc6ff001980a4c24a
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-23 14:16:55 +0100

    Update beg.js

--- commands/beg.js
+++ commands/beg.js
@@ -5,7 +5,7 @@ module.exports = {
   name: "beg",
   async execute(message, args, client, profileData) {
     const randomNumber = Math.floor(Math.random() * 500) + 1;
-    let names = require("./names.js")
+    let names = require("../names.js")
     let randomNames = names[Math.floor(Math.random() * names.legnth)];
     
     const response = await profileModel.findOneAndUpdate(
