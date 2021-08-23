b2e24f20990c95b823224c3c477940a81aa01954
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-23 18:51:22 +0100

    Update steal.js

--- commands/steal.js
+++ commands/steal.js
@@ -8,7 +8,14 @@ module.exports = {
     const randomNumber = Math.floor(Math.random() * 500) + 1;
     let mentionedUser = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
     
-    if (mentionedUser.profileData.coins < randomNumber) { return message.channel.send(`${mentionedUser} doesn't enough coins for you to steal!`) }
+    if (mentionedUser.profileData.coins < randomNumber) { 
+      const response = await profileModel.findOneAndUpdate({userID: message.author.id,},{$inc: {coins: mentionedUser.profileData.coins,},});
+      const response2 = await profileModel.findOneAndUpdate({userID: mentionedUser.id,},{$inc: {coins: -mentionedUser.profileData.coins,},});
+      return message.channel.send(`${message.author}, You stole ${mentionedUser.profileData.coins} coins from **${mentionedUser.username}**!`) 
+    }
+    if (mentionedUser.profileData.coins < 1) {
+      return message.reply(`**${mentionedUser.username}** has no coins in their Wallet for you to steal!`)
+    }
     
     const response = await profileModel.findOneAndUpdate(
       {
