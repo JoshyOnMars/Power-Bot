20fc7a819f5a51e77542bc0b7ba6bc7d3b297674
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-24 00:03:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,11 @@ client.on('ready', () => {
 	console.log('Ready!');
 	client.user.setActivity(`starship`, { type: 'WATCHING' });
         fetch('https://fdo.rocketlaunch.live/json/launches/next/5')
-        .then(res => res.json()) .then(json => { console.log(json.result); })
+        .then(res => res.json()) 
+        .then(json => {
+        for (var i in json.result) {
+        console.log(i);
+        }})
 });
 
 client.on('guildMemberAdd', async member => {
