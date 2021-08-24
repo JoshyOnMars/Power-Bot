6a617906c70a7e8e4488de3003ee4559e9d62fe1
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-24 17:41:02 +0100

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -24,7 +24,7 @@ module.exports = {
 
         const filter = ({ author, content }) => message.author == author && chosenLocations.some((location) => location.toLowerCase() == content.toLowerCase());
 
-        const collector = message.channel.createMessageCollector(filter, { max: 1, time: 25000 });
+        const collector = message.channel.createMessageCollector({ filter, time: 25000 });
 
         const earnings = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
 
