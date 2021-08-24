74290bd3df1548f4a6b791c861818787b549450f
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-24 11:48:53 +0100

    Update bank.js

--- commands/bank.js
+++ commands/bank.js
@@ -6,22 +6,22 @@ module.exports = {
   name: "bank",
   async execute(message, args, client, profileData) {
    
-    const number = args[0]
-    if (!number) { return message.channel.send(`Please specify the amount you want to deposit! (Min 1000)`)}
-    if (number > 1000) { return message.channel.send("Your amount is more than the min amount of 1000.")}
-    if (profileData.bank > 999) { return message.channel.send(`You have the max amount of \`1000\` in your bank!`)}
-    if (profileData.coins < 1) { return message.channel.send(`You have no coins to deposit into your bank!`)}
-    const response = await profileModel.findOneAndUpdate(
-      {
-        userID: message.author.id,
-      },
-      {
-        $inc: {
-          coins: -number,
-          bank: number,
-        },
-      }
-    );
-  return message.channel.send(`Banked ${number} coins into your Bank!`)
+    const number = args[1]
+    const input = args[0]
+                      
+    if (input === "deposit") {
+        if (!number) { return message.channel.send(`Please specify the amount you want to deposit! (Min 1000 coins)`)}
+        if (number > 1000) { return message.channel.send("Your amount is more than the min amount of 1000 coins.")}
+        if (profileData.bank > 999) { return message.channel.send(`You have the max amount of \`1000\` coins in your bank!`)}
+        if (profileData.coins < 1) { return message.channel.send(`You have no coins to deposit into your bank!`)}
+        const response = await profileModel.findOneAndUpdate({userID: message.author.id,},{$inc: {coins: -number,bank: number,},});
+        return message.channel.send(`Deposited ${number} coins from your Wallet into your Bank!`)
+    } else if (input === "withdraw) {
+        if (!number) { return message.channel.send(`Please specify the amount you want to withdraw! (Min 1000 coins)`)}
+        if (number > 1000) { return message.channel.send("Your amount is more than the min amount of 1000 coins.")}
+        if (profileData.bank < 1) { return message.channel.send(`You have no coins to withdraw from your bank!`)}
+        const response = await profileModel.findOneAndUpdate({userID: message.author.id,},{$inc: {coins: number,bank: -number,},});
+        return message.channel.send(`Withdrew ${number} coins from your Bank into your Wallet!`)
+    }
   },
 };
