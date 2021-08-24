026fdbcf2a06950545fbf1d2abe3d84548af4aa2
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-24 12:01:56 +0100

    Update launches.js

--- commands/launches.js
+++ commands/launches.js
@@ -2,17 +2,12 @@ const { MessageEmbed } = require('discord.js');
 const fetch = require("node-fetch");
 
 module.exports = {
-	name: 'launches',
-	description: 'Tells you the 5 upcoming rocket launches!',
-        category: 'Info',
-	async execute(message, args, client) {
-        
-        fetch('https://fdo.rocketlaunch.live/json/launches/next/25')
-        .then(res => res.json()) 
-        .then(json => {
-        for (const i of json.result) {
-        message.channel.send(`${i.provider.name}, ${i.vehicle.name}`);
-        }})
-        
-	},
+  name: 'launches',
+  description: 'Tells you the 5 upcoming rocket launches!',
+  category: 'Info',
+  async execute(message, args, client) {
+    const response = await fetch('https://fdo.rocketlaunch.live/json/launches/next/25');
+    const {result} = await res.json();
+    for (const {provider, vehicle} of result)  message.channel.send(`${provider.name}, ${vehicle.name}`);
+  },
 };
