4f4114afbb8b90a3e766d01e80bd471962349c42
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-24 13:00:27 +0100

    Update bal.js

--- commands/bal.js
+++ commands/bal.js
@@ -3,11 +3,14 @@ const { MessageEmbed } = require("discord.js")
 module.exports = {
   name: "bal",
   async execute(message, args, client, profileData) {
+    let user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author;
+    const bal = await profileModel.findOne({ userID: `${user.id}` });
+    
     let embed = new MessageEmbed()
     .setColor("RANDOM")
-    .setAuthor(`${message.author.username}'s Balance`)
-    .addField(`💰Wallet`, `${profileData.coins}`)
-    .addField(`🏦Bank`, `${profileData.bank}/1000`)
+    .setAuthor(`${user.username}'s Balance`)
+    .addField(`💰Wallet`, `${bal.coins}`)
+    .addField(`🏦Bank`, `${bal.bank}/1000`)
     .setTimestamp()
     message.channel.send({ embeds: [embed] });
   },
