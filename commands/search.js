7ac017e3de16ea9611bd9637f6c311f341bdaae3
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-24 17:51:29 +0100

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -30,7 +30,7 @@ module.exports = {
 
 
         collector.on('collect', async (m) => {
-            message.channel.send(`You found ${earnings} coins!`);
+            message.channel.send(`${message.author}, You picked \`${message.content}\` and found ${earnings} coins!`);
 
             await profileModel.findOneAndUpdate(
                 {
