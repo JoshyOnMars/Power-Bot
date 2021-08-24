c6395052898198063310f8d45195af0306953541
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-24 19:51:40 +0100

    Update steal.js

--- commands/steal.js
+++ commands/steal.js
@@ -15,7 +15,7 @@ module.exports = {
     if (bal.coins < randomNumber) { 
       const response = await profileModel.findOneAndUpdate({userID: message.author.id,},{$inc: {coins: bal.coins,},});
       const response2 = await profileModel.findOneAndUpdate({userID: mentionedUser.id,},{$inc: {coins: -bal.coins,},});
-      return message.channel.send(`${message.author}, You stole ${bal.coins} coins from **${mentionedUser.username}**!`) 
+      return message.reply(`${message.author}, You stole ${bal.coins} coins from **${mentionedUser.username}**!`) 
     }
     if (bal.coins < 1) {
       return message.reply(`**${mentionedUser.username}** has no coins in their Wallet for you to steal!`)
@@ -42,6 +42,6 @@ module.exports = {
       }
     );
     
-    return message.channel.send(`${message.author}, You stole ${randomNumber} coins from **${mentionedUser.username}**!`);
+    return message.reply(`${message.author}, You stole ${randomNumber} coins from **${mentionedUser.username}**!`);
   },
 };
