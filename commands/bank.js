4a74c6590c6250ce5f10113b58ceeb607e13367b
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-25 14:41:11 +0100

    Update bank.js

--- commands/bank.js
+++ commands/bank.js
@@ -13,24 +13,24 @@ module.exports = {
     if (!input) { return message.reply(`Please specify what you want to do, \`.bank withdraw/deposit <amount>\``)}
                       
     if (input === "deposit") {
-        if (!number) { return message.reply(`Please specify the amount you want to deposit! (Min 1000 coins)`)}
-        if (number > 1000) { return message.reply("Your amount is more than the min amount of 1000 coins.")}
-        if (profileData.bank > 999) { return message.reply(`You have the max amount of \`1000\` coins in your bank!`)}
+        if (!number) { return message.reply(`Please specify the amount you want to deposit! (Min 1,000 coins)`)}
+        if (number > 1000) { return message.reply("Your amount is more than the min amount of 1,000 coins.")}
+        if (profileData.bank > 999) { return message.reply(`You have the max amount of \`1,000\` coins in your bank!`)}
         if (profileData.coins < 1) { return message.reply(`You have no coins to deposit into your bank!`)}
         const response = await profileModel.findOneAndUpdate({userID: message.author.id,},{$inc: {coins: -number,bank: number,},});
         let embed = new MessageEmbed()
         .setColor("RANDOM")
-        .setDescription(`Deposited ${number} coins from your Wallet into your Bank!`)
+        .setDescription(`Deposited ${number.toLocaleString()} coins from your Wallet into your Bank!`)
         .setTimestamp()
         return message.reply({ embeds: [embed] })
     } else if (input === "withdraw") {
-        if (!number) { return message.reply(`Please specify the amount you want to withdraw! (Min 1000 coins)`)}
-        if (number > 1000) { return message.reply("Your amount is more than the min amount of 1000 coins.")}
+        if (!number) { return message.reply(`Please specify the amount you want to withdraw! (Min 1,000 coins)`)}
+        if (number > 1000) { return message.reply("Your amount is more than the min amount of 1,000 coins.")}
         if (profileData.bank < 1) { return message.reply(`You have no coins to withdraw from your bank!`)}
         const response = await profileModel.findOneAndUpdate({userID: message.author.id,},{$inc: {coins: number,bank: -number,},});
         let embed2 = new MessageEmbed()
         .setColor("RANDOM")
-        .setDescription(`Withdrew ${number} coins from your Bank into your Wallet!`)
+        .setDescription(`Withdrew ${number.toLocaleString()} coins from your Bank into your Wallet!`)
         .setTimestamp()
         return message.reply({ embeds: [embed2]})
     }
