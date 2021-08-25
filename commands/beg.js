1ee703a0994f67d2a344700bb29ff59c072690d1
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-25 01:49:52 +0100

    Update beg.js

--- commands/beg.js
+++ commands/beg.js
@@ -20,7 +20,7 @@ module.exports = {
                   ]
     let randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
 
-    if (randomPhrase == `${phrases[1]}` || `${phrases[2]}`) {
+    if (randomPhrase.includes(phrases[1]) || randomPhrase.includes(phrases[2])) {
          let lose = new MessageEmbed()
          .setColor("RED")
          .setTitle(`${randomName}`)
