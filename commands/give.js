b1d5a46d4b55f805bb66e285738ef9d1cc28cc3b
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-25 14:25:41 +0100

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -6,9 +6,12 @@ module.exports = {
   category: "Currency",
   cooldown: 15,
   async execute(message, args, client, profileData) {
-
-    let num = args[1]
+    
     let mentionedUser = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+    if (!mentionedUser) { return message.reply(`Please provide a user you want to give money to! Like this: \`.give [user] <amount>\``) }
+    
+    let num = args[1]
+    if (!num) { return message.reply(`Please provide the amount of money you want to give! Like this: \`.give [user] <amount>\``) }
     
     if (profileData.coins < num) {
       return message.reply(`You don't have ${num} coins to give to **${mentionedUser.username}**!`)
@@ -35,6 +38,6 @@ module.exports = {
       }
     );
     
-    return message.reply(`${message.author}, You gave ${num} coins to **${mentionedUser.username}**!`);
+    return message.reply(`You gave **${num} coins** to **${mentionedUser.username}**!`);
   },
 };
