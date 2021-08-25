688cf654d6ab12d8e5afc156055a558ede7d6ea1
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-25 14:45:31 +0100

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -10,11 +10,11 @@ module.exports = {
     let mentionedUser = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
     if (!mentionedUser) { return message.reply(`Please provide a user you want to give money to! Like this: \`.give [user] <amount>\``) }
     
-    let num = args[1].toLocaleString()
+    let num = args[1]
     if (!num) { return message.reply(`Please provide the amount of money you want to give! Like this: \`.give [user] <amount>\``) }
     
     if (profileData.coins < num) {
-      return message.reply(`You don't have ${num} coins to give to **${mentionedUser.username}**!`)
+      return message.reply(`You don't have **${num.toLocaleString()} coins** to give to **${mentionedUser.username}**!`)
     }
     
     const response = await profileModel.findOneAndUpdate(
@@ -38,6 +38,6 @@ module.exports = {
       }
     );
     
-    return message.reply(`You gave **${num} coins** to **${mentionedUser.username}**!`);
+    return message.reply(`You gave **${num.toLocaleString()} coins** to **${mentionedUser.username}**!`);
   },
 };
