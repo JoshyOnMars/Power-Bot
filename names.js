24acd74d880c1406368fe1abdb7edda273be53d0
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-25 02:11:56 +0100

    Update names.js

--- names.js
+++ names.js
@@ -1,1 +1,21 @@
-module.exports = ["Elon Musk", "Jeff Bezos", "Bill Gates", "Joe Biden", "Zac Aubert", "Marcus"]
+module.exports = [
+"Elon Musk", 
+"Jeff Bezos", 
+"Bill Gates", 
+"Joe Biden", 
+"Zac Aubert",
+"Mr Beast",
+"KSI",
+"Logan Paul",
+"Jake Paul",
+"Man In A Van",
+"DanTDM",
+"Mom",
+"Dad",
+"Sister",
+"Brother",
+"Ninja",
+"John Cena",
+"Some Stranger",
+"Mark Zuckerberg"
+]
