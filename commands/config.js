d46a403f137fc71d6b4ac7720296e0386d8326ee
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-26 21:55:56 +0100

    Update config.js

--- commands/config.js
+++ commands/config.js
@@ -3,6 +3,7 @@ const serverModel = require("../models/serverSchema");
 
 module.exports = {
   name: "config",
+  usage: ".config logchannel <channel>",
   category: "Moderation",
   async execute(message, args, client, serverData) {
     if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.reply(`You don't have the permission \`MANAGE_MESSAGES\` to run this command!`)
