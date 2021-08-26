91e8b643b4e196730c595a450be57520b045a533
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-26 21:17:11 +0100

    Update faq.js

--- commands/faq.js
+++ commands/faq.js
@@ -3,7 +3,6 @@ const { MessageEmbed } = require("discord.js")
 module.exports = {
 	  name: 'faq',
           description: "Find your answers you're looking for with this command!",
-    	  category: 'Info',
 	  async execute(message, args, client) {
 		  
 	if (!message.guild.id === "793016551682867250") return;
