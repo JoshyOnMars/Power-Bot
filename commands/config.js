23b82412915f732f12796f8a0c1d004275d13fe7
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-27 11:11:52 +0100

    Update config.js

--- commands/config.js
+++ commands/config.js
@@ -8,9 +8,6 @@ module.exports = {
   async execute(message, args, client, serverData) {
     if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.reply(`You don't have the permission \`MANAGE_MESSAGES\` to run this command!`)
     
-    //let mentionedUser = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
-    //if (!mentionedUser) { return message.reply(`Please provide a user you want to give money to! Like this: \`.give [user] <amount>\``) }
-    
     let input = args[0]
     if (!input) return message.reply(`Well.. What do you want to do?`);
     
@@ -18,8 +15,16 @@ module.exports = {
     if (input === "logchannel") {
           let logchannel = message.mentions.channels.first()
           if (!logchannel) return message.reply(`Please mention the channel you want to use as a logging channel.`);
-      await serverModel.findOneAndUpdate({serverID: message.guild.id,},{logChannel: logchannel.id})
-      message.reply(`Set logging channel to ${logchannel}`)
+          await serverModel.findOneAndUpdate({serverID: message.guild.id,},{logChannel: logchannel.id})
+          message.reply(`Set logging channel to ${logchannel}`)
+    } else if (input === "prefix") {
+        if(!args[1]) return message.channel.send("Provide a prefix!");
+        if(args[0] === ".") return await serverData.deleteOne({prefix: "."}).then(message.channel.send("Prefix has been set to default"));
+        const prefixEmbed = new Discord.MessageEmbed().setColor("YELLOW").setTitle("Prefix Changed!").setDescription(`My refix for this server has been changed to \`${args[1]}\`.`)
+        await serverData.findOneAndUpdate({serverID: message.guild.id,},{prefix: `${args[1]}`})
+        return message.channel.send({ embeds: [prefixEmbed] });
+      }
+      };
     }
   },
 };
