c9cd7b3846c22852251285307c1cb2209a46d021
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-27 11:24:06 +0100

    Update config.js

--- commands/config.js
+++ commands/config.js
@@ -20,7 +20,7 @@ module.exports = {
     } else if (input === "prefix") {
           if(!args[1]) return message.channel.send("Provide a prefix!");
           if(args[0] === ".") return await serverModel.deleteOne({prefix: "."}).then(message.channel.send("Prefix has been set to default"));
-          const prefixEmbed = new Discord.MessageEmbed().setColor("YELLOW").setTitle("Prefix Changed!").setDescription(`My prefix for this server has been changed to \`${args[1]}\`.`)
+          const prefixEmbed = new MessageEmbed().setColor("YELLOW").setTitle("Prefix Changed!").setDescription(`My prefix for this server has been changed to \`${args[1]}\`.`)
           await serverModel.findOneAndUpdate({serverID: message.guild.id,},{prefix: `${args[1]}`})
           return message.channel.send({ embeds: [prefixEmbed] });
     }
