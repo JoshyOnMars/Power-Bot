055980ed72c649dd5380b67cee6793bd16ae93f7
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-28 17:12:55 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,30 +3,32 @@ const { pagination } = require("reconlx")
 const profileModel = require("../models/profileSchema");
 
 module.exports = {
-  name: "help",
-  async execute(message, args, client, profileData) {
-	  
- 		let embed = new MessageEmbed()
-	  
-	  	function chunk(array, chunkSize){
- 	  	const chunked = [];
-          	for(let i = 0; i < array.length; i += chunkSize)
-    	  	chunked.push(array.slice(i, i + chunkSize));
- 	  	return chunked;
-	  	}
-	  
-		for(const [name, {description}] of client.commands){
-			embed.addField(`**${name}**`, `<:smth:881147807879286804> ${description || "none"}`)
-		}
-	  
-	  	const chunkedArray = chunk(embed.fields, 10);
-	  
-	  	pagination({
-			embeds: chunkedArray,
-			channel: message.channel,
-			author: message.author,
-			time: 15000,
-		})
-    
-  },
+    name: "help",
+    async execute(message, args, client, profileData) {
+        const embedPages = [];
+      
+        function chunk(array, chunkSize){
+            const chunked = [];
+            for(let i = 0; i < array.length; i += chunkSize) {
+                chunked.push(array.slice(i, i + chunkSize));
+            }
+            return chunked;
+        }
+      
+        for(const [name, {description}] of client.commands){
+            let embed = new MessageEmbed();
+            embed.addField(`**${name}**`, `:smth: ${description || "none"}`);
+
+            embedPages.push(embed);
+        }
+      
+        const chunkedArray = chunk(embedPages, 10);
+      
+        pagination({
+            embeds: chunkedArray,
+            channel: message.channel,
+            author: message.author,
+            time: 15000,
+        });
+    },
 };
