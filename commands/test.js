325d91245a8e7bd37e45441aa04ab7bd4a6f9d9a
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-28 13:00:00 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -6,31 +6,10 @@ module.exports = {
   name: "test",
   async execute(message, args, client, profileData) {
 	  
-	const wait = require('util').promisify(setTimeout);
-
-	const row = new MessageActionRow()
-		.addComponents(
-		new MessageButton()
-		.setCustomId('primary')
-		.setLabel('Primary')
-		.setStyle('PRIMARY')
-		);
-
-	await message.reply({ content: 'Pong!', components: [row] });
+	  let embed = new MessageEmbed()
+	  .setDescription(`**help**\n<:smth:881145180160413756>Get some help!`)
 	  
-	const filter = i => i.customId === 'primary' && i.user.id === '691634056278048778';
-
-	const collector = message.channel.createMessageComponentCollector({ filter, time: 15000 });
-	  
-	collector.on('collect', async i => {
-	if (i.customId === 'primary') {
-		await i.deferUpdate();
-		await wait(4000);
-		await i.editReply({ content: 'A button was clicked!', components: [] });
-	}
-	});
-
-	collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+	  message.channel.send({ embeds: [embed] })
     
   },
 };
