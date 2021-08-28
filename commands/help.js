ad94cee218975e2980f82b0c4f464dba4597bcc4
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-28 17:46:12 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,24 +5,33 @@ const profileModel = require("../models/profileSchema");
 module.exports = {
     name: "help",
     async execute(message, args, client, profileData) {
-        const embedPages = [];
-      
-        function chunk(array, chunkSize){
+        // Chunk array in smaller chunks
+        // Default is set to '15'
+        const chunk = (array, chunkSize = 15) => {
             const chunked = [];
             for(let i = 0; i < array.length; i += chunkSize) {
                 chunked.push(array.slice(i, i + chunkSize));
             }
+
             return chunked;
         }
-      
-        for(const [name, {description}] of client.commands){
-            let embed = new MessageEmbed();
-            embed.addField(`**${name}**`, `:smth: ${description || "none"}`);
 
-            //embedPages.push(embed);
+        // This is used by the library
+        const embedPages = [];
+
+        // Store all client commands in this array.
+        const clientCommands = [];
+        for(const [name, {description}] of client.commands) {
+            clientCommands.push({ name: name, value: `:smth: ${description || "none"}` });
         }
-      embedPages.push(embed);
-        // const chunkedArray = chunk(embedPages, 10);
+        // Chunking the commands in smaller arrays. Leave second parameter blank to use default of 15, or choose your own size.
+        // Note that Discord doesn't allow any embed to go over 25 fields, 2000 characters.
+        chunk(clientCommands, 10).forEach((chunks) => {
+            const embed = new MessageEmbed().setTitle("Available commands").setDescription("The commands below are available to be used in your server.");
+            embed.addFields(chunks);
+            embedPages.push(embed);
+        });
+
       
         pagination({
             embeds: embedPages,
