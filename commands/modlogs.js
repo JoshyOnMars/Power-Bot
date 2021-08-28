ebc571f1cd89ca5dbdca089775201b2c4b944d61
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-28 19:26:24 +0100

    Update modlogs.js

--- commands/modlogs.js
+++ commands/modlogs.js
@@ -2,12 +2,13 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = {
   name: "modlogs",
-  async execute(message, args, client, profileData) {
-    let embed = new MessageEmbed()
-    .setColor("RANDOM")
-    .setAuthor(`${message.author.username}'s Modlogs`)
-    .addField(`Modlogs`, `\`${profileData.modLogs}\``)
-    .setTimestamp()
-    message.channel.send({ embeds: [embed] });
+  async execute(message, args, client, profileData, serverData) {
+    
+    //let embed = new MessageEmbed()
+    //.setColor("RANDOM")
+    //.setAuthor(`${message.author.username}'s Modlogs`)
+    //.addField(`Modlogs`, `\`${profileData.modLogs}\``)
+    //.setTimestamp()
+    //message.channel.send({ embeds: [embed] });
   },
 };
