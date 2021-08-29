0abaf479c0de4c54d42f6c5a8000298c74780068
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-29 11:11:30 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -8,11 +8,18 @@ module.exports = {
 	async execute(message, args, client) {
 
         const Ping = `${Math.round(message.client.ws.ping)}ms`
-    
+    	let totalSeconds = (client.uptime / 1000);
+    	let days = Math.floor(totalSeconds / 86400);
+    	let hours = Math.floor(totalSeconds / 3600);
+    	totalSeconds %= 3600;
+    	let minutes = Math.floor(totalSeconds / 60);
+    	let seconds = Math.floor(totalSeconds % 60);
+
+    let uptime = `${days}D:${hours}H:${minutes}M:${seconds}S`;
         const Embed = new MessageEmbed()
         .setColor("#2f3136")
         .setTitle("🏓 Pong!")
-        .setDescription(`Ping is ${Ping}`)
+        .setDescription(`Ping is ${Ping}\nUptime is ${uptime}`)
 
 		await message.reply({ embeds: [Embed] });
 	},
