b2fb75b7ed8e77546c3cf758127012fdfdece905
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-08-29 15:18:37 +0100

    Create eval.js

---
+++ commands/eval.js
@@ -0,0 +1,57 @@
+const { MessageEmbed } = require("discord.js");
+let moment = require("moment")
+
+module.exports = {
+  name: "eval",
+  async execute(message, args, client) {
+    function clean(text) {
+      if (typeof text === "string")
+        return text
+          .replace(/`/g, "`" + String.fromCharCode(8203))
+          .replace(/@/g, "@" + String.fromCharCode(8203));
+      else return text;
+    }
+
+    const args = message.content.split(" ").slice(1);
+
+    {
+      if (message.author.id !== "691634056278048778")
+        return message.reply":x: You are not my owner!");
+      try {
+        const code = args.join(" ");
+        let evaled = eval(code);
+
+        if (typeof evaled !== "string")
+          evaled = require("util").inspect(evaled);
+
+        let evalEmbed = new MessageEmbed()
+        .setColor("#2f3136")
+          // .setDescription(`\`\`\`xl\n${clean(evaled)}\`\`\``)
+          .addFields(
+            {
+              name: "📥INPUT📥",
+              value: `\`\`\`js\n${code}\`\`\``,
+              inline: false
+            },
+            {
+              name: "📤OUTPUT📤",
+              value: `\`\`\`xl\n${clean(evaled)}\n\`\`\``,
+              inline: false
+            }
+          );
+        message.reply({ embeds: [evalEmbed] });
+      } catch (err) {
+        let errEmbed = new MessageEmbed()
+        .setColor("#2f3136")
+        .addFields({
+          name: "ERROR",
+          value: `\`\`\`xl\n${clean(err)}\n\`\`\``,
+          inline: false
+        });
+
+        message.reply({ embeds: [errEmbed] });
+      }
+    }
+  }
+};
+//clean(evaled), { code: "xl" }
