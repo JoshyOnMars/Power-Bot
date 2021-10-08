6d0dafe2527c5651516b7504caca92eea8b22e19
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-08 18:05:50 +0100

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
 	name: 'add',
 	usage: "add",
-  	category: 'Economy',
+  	category: 'Currency',
 	async execute(message, args, client) {
         if (message.author.id != "691634056278048778") return;
         
