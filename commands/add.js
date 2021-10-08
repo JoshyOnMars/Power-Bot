0d06288dc9d9dcdde374f298a8a22dd060d3212f
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-08 16:14:44 +0100

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -5,7 +5,16 @@ module.exports = {
 	usage: "add",
   	category: 'Economy',
 	async execute(message, args, client) {
-        const member = 
-        
+        if (message.author.id != "691634056278048778") return;
+
+
+        let member = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+        if (!member) { return message.reply(`Please provide a user you want to add money to! Like this: \`.add [user] <amount>\``) }
+    
+        let amount = args[1]
+        if (!amount) { return message.reply(`Please provide the amount of money you want to add! Like this: \`.add [user] <amount>\``) }
+
+        client.add(member.id, amount)
+
 	},
 };
