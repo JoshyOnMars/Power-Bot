327823b2a72d1b69ed0f2a3adfa3e4f8cfe7c3b0
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-08 16:12:01 +0100

    Create add.js

---
+++ commands/add.js
@@ -0,0 +1,11 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	name: 'add',
+	usage: "add",
+  	category: 'Economy',
+	async execute(message, args, client) {
+        const member = 
+        
+	},
+};
