d30c3e2c50559491f3dbb9a4b5bdf8b0a1ddb15a
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-09 13:36:22 +0100

    Update items.js

--- items.js
+++ items.js
@@ -1,10 +1,12 @@
 module.exports = [
       {
             item: "apple",
+            description: "One of your Five a day!",
             price: 5,
       },
       {
             item: "cookie",
+            description: "Nice and sweet!",
             price: 10,
       }
 ]
