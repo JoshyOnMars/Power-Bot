242127431507b27bdcca7a4f75855cec4084c9e8
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-09 14:43:29 +0100

    Update inventory.js

--- commands/inventory.js
+++ commands/inventory.js
@@ -7,19 +7,16 @@ module.exports = {
   category: "Currency",
   async execute(message, args, client) {
     inventoryModel.findOne(
-      { guildID: message.guild.id, userID: message.author.id },
-      async (err, data) => {
+      { guildID: message.guild.id, userID: message.author.id }, async (err, data) => {
         if (!data) return message.reply("You have no owned items dummy!");
-        const mappedData = Object.keys(data.inventory)
-          .map((key) => {
+        const mappedData = Object.keys(data.inventory).map((key) => {
             return `**${key}** — ${data.inventory[key]}`;
-          }).join("\n\n")
+        })}).join("\n\n")
         
         let embed = new MessageEmbed()
         .setTitle(`Owned Items`)
         .setDescription(`${mappedData}`)
         
         message.channel.send({ embeds: [embed]})
-      })
       }),
 };
