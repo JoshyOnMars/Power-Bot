15b7b879d21ccdf55529a47417b500438265d22a
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-09 11:39:05 +0100

    Update items.js

--- items.js
+++ items.js
@@ -1,3 +1,13 @@
 module.exports = [
-{item: "apple", cost: 5}
+      ["item1", "itemdesc1", 5],
+      ["item2", "itemdesc2", 10],
+      ["item3", "itemdesc3", 15],
+      ["item4", "itemdesc4", 20],
+      ["item5", "itemdesc5", 25],
+      ["item6", "itemdesc6", 30],
+      ["item7", "itemdesc7", 35],
+      ["item8", "itemdesc8", 40],
+      ["item9", "itemdesc9", 45],
+      ["item10", "itemdesc10", 50],
+      ["item11", "itemdesc11", 55],
 ]
