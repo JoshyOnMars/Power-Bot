72c9f67a7a6e0f9afda68930089f5258129d019d
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-09 11:40:37 +0100

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -1,25 +1,12 @@
 const { MessageEmbed } = require("discord.js");
 const { pagination } = require("reconlx")
+const shopItems = require("../items.js")
 
 module.exports = {
   name: "shop",
   category: "Currency",
   cooldown: 5,
   async execute(message, args, client) {
-
-    let shopItems = [
-      ["item1", "itemdesc1", 5],
-      ["item2", "itemdesc2", 10],
-      ["item3", "itemdesc3", 15],
-      ["item4", "itemdesc4", 20],
-      ["item5", "itemdesc5", 25],
-      ["item6", "itemdesc6", 30],
-      ["item7", "itemdesc7", 35],
-      ["item8", "itemdesc8", 40],
-      ["item9", "itemdesc9", 45],
-      ["item10", "itemdesc10", 50],
-      ["item11", "itemdesc11", 55],
-    ];
     
         // Chunk array in smaller chunks
         // Default is set to '15'
