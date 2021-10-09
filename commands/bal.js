2d4648a907c3399a3d314e86b22e6c854aa02e0a
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-09 08:10:37 +0100

    Update bal.js

--- commands/bal.js
+++ commands/bal.js
@@ -1,21 +1,21 @@
 const { MessageEmbed } = require("discord.js")
-const profileModel = require("../models/profileSchema");
+const moneyModel = require("../models/moneySchema");
 
 module.exports = {
   name: "bal",
   usage: "bal",
   description: "Check yours or someone elses wallet and bank balance!",
   category: "Currency",
-  async execute(message, args, client, profileData, serverData) {
+  async execute(message, args, client, moneyData, serverData) {
     if (serverData.economy == false) return message.reply(`The module \`economy\` is **disabled**`);
     let user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author;
-    const bal = await profileModel.findOne({ userID: `${user.id}` });
+    const bal = await moneyModel.findOne({ userID: user.id });
     
     let embed = new MessageEmbed()
     .setColor("RANDOM")
     .setAuthor(`${user.username}'s Balance`)
     .addField(`💰Wallet`, `${bal.coins.toLocaleString()}`)
-    .addField(`🏦Bank`, `${bal.bank.toLocaleString()}/${bal.bankSize.toLocaleString()}`)
+    //.addField(`🏦Bank`, `${bal.bank.toLocaleString()}/${bal.bankSize.toLocaleString()}`)
     .setTimestamp()
     message.reply({ embeds: [embed] });
   },
