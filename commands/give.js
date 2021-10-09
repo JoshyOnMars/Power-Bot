7b49c4818691f9245095782e9734771e522d986c
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-09 08:13:42 +0100

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -1,5 +1,5 @@
 const { MessageEmbed } = require("discord.js")
-const profileModel = require("../models/profileSchema");
+//const profileModel = require("../models/profileSchema");
 
 module.exports = {
   name: "give",
@@ -7,7 +7,7 @@ module.exports = {
   description: "Be nice and give someone money :)",
   category: "Currency",
   cooldown: 15,
-  async execute(message, args, client, profileData, serverData) {
+  async execute(message, args, client, moneyData, serverData) {
     if (serverData.economy == false) return message.reply(`The module \`economy\` is **disabled**`);
     
     let mentionedUser = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
@@ -16,7 +16,7 @@ module.exports = {
     let num = args[1]
     if (!num) { return message.reply(`Please provide the amount of money you want to give! Like this: \`.give [user] <amount>\``) }
     
-    if (profileData.coins < num) {
+    if (moneyData.coins < num) {
       return message.reply(`You don't have **${num.toLocaleString()} coins** to give to **${mentionedUser.username}**!`)
     }
     
