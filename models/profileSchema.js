b1ee535dc3ae4f03073ee5c93f78f9b3ca1977e1
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-09 08:05:42 +0100

    Update profileSchema.js

--- models/profileSchema.js
+++ models/profileSchema.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
-const profileSchema = new mongoose.Schema({
+const moneySchema = new mongoose.Schema({
   userID: { type: String, require: true, unique: true },
   coins: { type: Number, default: 1000 },
-  bank: { type: Number },
-  bankSize: { type: Number, default: 1000 },
 });
 
-const model = mongoose.model("ProfileModels", profileSchema);
+const model = mongoose.model("Money", moneySchema);
 
 module.exports = model;
