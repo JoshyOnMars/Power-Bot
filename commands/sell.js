30cca086a361a11f2f926ed4a55ef2fe5a45bf7a
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-09 16:47:30 +0100

    Create sell.js

---
+++ commands/sell.js
@@ -0,0 +1,34 @@
+const { MessageEmbed } = require('discord.js');
+const inventoryModel = require('../models/inventorySchema.js')
+const shopItems = require('../items.js')
+
+module.exports = {
+	name: 'sell',
+	usage: "sell",
+  	category: 'Currency',
+	async execute(message, args, client, inventoryData) {
+		
+        if(!args[0]) return message.reply("Please state an item you want to sell!");
+        const itemToSell = args[0].toLowerCase()
+        
+        const validItem = !!inventoryData.inventory.find((val) => val.item.toLowerCase() === itemToSell);
+        if(!validItem) return message.reply(`You have no item called \`${itemToSell}\`, Please try again... CORRECTLY!`);
+        
+        const itemPrice = shopItems.find((val) => (val.item.toLowerCase()) === itemToSell).price;
+        
+        const params = {
+          userID: message.author.id
+        }
+        inventoryModel.findOne(params, async(err, data) => {
+          if(data) {
+            const hasItem = Object.keys(data.inventory).includes(itemToSell)
+              data.inventory[itemToSell]--
+            }
+          console.log(data)
+          await inventoryModel.findOneAndUpdate(params, data);
+         }
+         message.reply(`Sold \`${itemToSell}\` for ${itemPrice} coins!`)
+	      client.add(message.author.id, parseInt(itemPrice / 1.5), 0)
+        });
+	},
+};
