b74c324c2c880a4d191197bce68f105f14076679
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-09 08:44:31 +0100

    Update moneySchema.js

--- models/moneySchema.js
+++ models/moneySchema.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const moneySchema = new mongoose.Schema({
   userID: { type: String, require: true, unique: true },
   coins: { type: Number, default: 1000 },
+  bank: { type: Number, default: 0 },
 });
 
 const model = mongoose.model("Money", moneySchema);
