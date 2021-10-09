05854e4dd8c94a1ba819dd8a0756f87bf74e051e
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-09 13:25:21 +0100

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -10,7 +10,7 @@ module.exports = {
     if(shopItems.length === 0) return message.reply("No items are for sale!");
     
     const shopList = shopItems.map((value, index) => {
-      return `**${index + 1}** ${value.item} - ${value.price} coins\n`;
+      return `**${value.item}** - \`${value.price}\``;
     });
     
     message.channel.send({ content: `${shopList}` })
