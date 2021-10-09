5a0a106d03dad0c767702f81be19acfd1f89308e
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-09 13:10:32 +0100

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -7,41 +7,12 @@ module.exports = {
   category: "Currency",
   cooldown: 5,
   async execute(message, args, client) {
+    if(shopItems.length === 0) return message.reply("No items are for sale!");
     
-        // Chunk array in smaller chunks
-        // Default is set to '15'
-        const chunk = (array, chunkSize = 15) => {
-            const chunked = [];
-            for(let i = 0; i < array.length; i += chunkSize) {
-                chunked.push(array.slice(i, i + chunkSize));
-            }
-
-            return chunked;
-        }
-
-        // This is used by the library
-        const embedPages = [];
-
-        // Store all client commands in this array.
-        const itemName = [];
-        for(const item of shopItems) {
-            itemName.push({ name: `${item[0]} - ${item[2]} coins`, value: item[1] });
-        }
-        // Chunking the commands in smaller arrays. Leave second parameter blank to use default of 15, or choose your own size.
-        // Note that Discord doesn't allow any embed to go over 25 fields, 2000 characters.
-        chunk(itemName, 6).forEach((chunks) => {
-            const embed = new MessageEmbed().setColor("YELLOW").setAuthor(`Shop`, client.user.displayAvatarURL())
-            embed.addFields(chunks);
-            embedPages.push(embed);
-        });
-
-
+    const shopList = shopItems.map((value, index) => {
+      return `**${index + 1}** ${value.item} - ${value.price} coins`
+    });
     
-      pagination({
-            embeds: embedPages,
-            channel: message.channel,
-            author: message.author,
-            time: 15000,
-        });
+    message.channel.send(shopList)
   },
 };
