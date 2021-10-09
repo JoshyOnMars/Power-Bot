e4f0d1185de5b87f2d1d66af902867005e980ee7
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-09 08:49:31 +0100

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -10,7 +10,7 @@ module.exports = {
         let member = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
         if (!member) { return message.reply(`Please provide a user you want to add money to! Like this: \`.add [user] <amount>\``) }
     
-        client.add(member.id, parseInt(args[1]))
+        client.add(member.id, parseInt(args[1]), parseInt(args[2]))
 
 	},
 };
