733c3d20695d85faf7eedad63fd8a8daf7d8201e
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-10 14:26:56 +0100

    Update double.js

--- commands/double.js
+++ commands/double.js
@@ -5,26 +5,26 @@ module.exports = {
 	usage: "double <amount>",
   	category: 'Currency',
 	async execute(message, args, client) {
-        if(!args[0]) return message.reply("Please sepcify the amount you want to bet!")
-        if(isNaN(args[0])) return message.reply("The argument must be a number!")
-    
-        const amountToBet = parseInt(args[0]);
-    
-      if(await client.bal(message.author.id) < amountToBet) return message.reply("You don't have enough coins to bet that amount!")
 		
-	client.remove(message.author.id, amountToBet, 0)
-    
-      function random() {
-        const num = Math.floor(Math.random() * 2);
-        return num === 1;
-      };
+        	if(!args[0]) return message.reply("Please sepcify the amount you want to bet!")
 		
-    if(random() === true) {
-      const winAmount = amountToBet * 2;
-      message.reply(`Congrats you won ${winAmount} coins!`)
-      client.add(message.author.id, winAmount, 0)
-    } else {
-      message.reply(`You lost ${amountToBet} coins, lmao`);
-    }
+        	if(isNaN(args[0])) return message.reply("The argument must be a number!")
+    
+        	const amountToBet = parseInt(args[0]);
+    
+      		if(await client.bal(message.author.id) < amountToBet) return message.reply("You don't have enough coins to bet that amount!")
+		
+		client.remove(message.author.id, amountToBet, 0)
+    
+      		let chances = ["win", "lose"];
+		let pick = chances[Math.floor(Math.random() * chances.length)]
+		
+    		if(pick == "lose") {
+      			message.reply(`You lost ${amountToBet} coins, lmao`);
+    		} else {
+      			const winAmount = amountToBet * 2;
+      			message.reply(`Congrats you won ${winAmount} coins!`)
+      			client.add(message.author.id, winAmount, 0)
+    		}
 	},
 };
