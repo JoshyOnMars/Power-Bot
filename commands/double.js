f1dd48b531f0a2274b1c2076e0d7a6cbe1d72bec
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-10 14:46:47 +0100

    Update double.js

--- commands/double.js
+++ commands/double.js
@@ -1,8 +1,8 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-	name: 'double',
-	usage: "double <amount>",
+	name: 'gamble',
+	usage: "gamble <amount>",
   	category: 'Currency',
 	async execute(message, args, client) {
 		
@@ -11,9 +11,6 @@ module.exports = {
         	if(isNaN(args[0])) return message.reply("The argument must be a number!")
 		
 		let amountToBet = parseInt(args[0]);
-		if (amountToBet) {
-		client.remove(message.author.id, amountToBet, 0)
-		}
     
       		if(await client.bal(message.author.id) < amountToBet) return message.reply("You don't have enough coins to bet that amount!")
     
@@ -22,8 +19,9 @@ module.exports = {
 		
     		if(pick == "lose") {
       			message.reply(`You lost ${amountToBet} coins, lmao`);
+			client.remove(message.author.id, amountToBet, 0)
     		} else {
-      			const winAmount = amountToBet * 2;
+      			const winAmount = amountToBet * Math.floor(Math.random() * 3);
       			message.reply(`Congrats you won ${winAmount} coins!`)
       			client.add(message.author.id, winAmount, 0)
     		}
